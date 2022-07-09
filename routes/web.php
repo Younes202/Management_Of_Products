893a28a25092318f6037a263743ed4da3ad4e458
“Younes <sghyeryounes@gmail.com>
2022-07-09 16:42:10 +0200

    controllers_routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+route::resource("Client","App\http\Controllers\client\ClientController",["only"=>["index","show"]]);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::resource("Products","App\http\Controllers\Products\ProductController",["only"=>["index","show"]]);
+
+route::resource("Sellers","App\http\Controllers\Sellers\SellerController",["only"=>["index","show"]]);
+
+route::resource("Users","App\http\Controllers\Users\UserController",["except"=>["create","edit"]]);
+
+route::resource("Categories","App\http\Controllers\Categories\CategoriesController",["except"=>["create","index"]]);
+
+route::resource("Transactions","App\http\Controllers\Transactions\TransactionController",["only"=>["index","show"]]);
